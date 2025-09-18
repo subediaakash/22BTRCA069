d76fc4816e1a3f972c288e0eb2bb91d00523e30a
spartan-readytocode <7007966033abc@gmail.com>
2025-09-18 11:22:38 +0530

    feat : db setup and hello world from backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,9 @@
 
+import express from 'express'
+import type { Request,Response } from 'express'
+
+const app = express()
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("helloworld")
+})
